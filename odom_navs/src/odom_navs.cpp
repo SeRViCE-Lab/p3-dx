30e309d4941c9d6a18b94708270f846227b064f6
lakehanne <patlekano@gmail.com>
2016-06-30 14:52:29 -0400

    nav stack without map, need to fix sensor origin

--- odom_navs/src/odom_navs.cpp
+++ odom_navs/src/odom_navs.cpp
@@ -11,19 +11,22 @@
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Vector3.h>
+#include <std_msgs/Float64.h>
 
 class odom
 {
 private:
-	geometry_msgs::Pose pose;
-	geometry_msgs::Twist twist;
+	geometry_msgs::PoseWithCovariance pose;
+	geometry_msgs::TwistWithCovariance twist;
+	std_msgs::Float64 poseCov;
+	std_msgs::Float64 twistCov;
 
 public:
 	odom(){}
 	~odom(){}
 
-	void readPose(const nav_msgs::Odometry::ConstPtr& odom_msg, geometry_msgs::Pose pose);
-	void readTwist(const nav_msgs::Odometry::ConstPtr& odom_msg, geometry_msgs::Twist twist);
+	void readPose(const nav_msgs::Odometry::ConstPtr& odom_msg, geometry_msgs::PoseWithCovariance pose);
+	void readTwist(const nav_msgs::Odometry::ConstPtr& odom_msg, geometry_msgs::TwistWithCovariance twist);
 
 	void odomCallback(const nav_msgs::Odometry::ConstPtr& odom_msg)
 	{
@@ -34,17 +37,26 @@ public:
 	
 };
 
-void odom::readPose(const nav_msgs::Odometry::ConstPtr& odom_msg, geometry_msgs::Pose pose)
+void odom::readPose(const nav_msgs::Odometry::ConstPtr& odom_msg, geometry_msgs::PoseWithCovariance pose)
 {
 	//x, y, z, and quarternion
-	// pose = odom_msg.pose;
-	// ROS_INFO_STREAM("odom pose " << odom_msg->pose);
+	pose = odom_msg->pose;
+	geometry_msgs::Point pose_posit = pose.pose.position;
+	geometry_msgs::Quaternion pose_orient = pose.pose.orientation;
+	// poseCov = pose.covariance;
+	// ROS_INFO("pose linear: (%f, %f, %f)", pose_posit.x, pose_posit.y, pose_posit.z);
+	// ROS_INFO("pose orient: (%f, %f, %f)", pose_orient.x, pose_orient.y, pose_orient.z);
+	// std::cout << "\n" << std::endl;
 }
 
-void odom::readTwist(const nav_msgs::Odometry::ConstPtr& odom_msg, geometry_msgs::Twist twist)
+void odom::readTwist(const nav_msgs::Odometry::ConstPtr& odom_msg, geometry_msgs::TwistWithCovariance twist)
 {
-	// twist = odom_msg.twist;
-	// ROS_INFO_STREAM("odom twist " << odom_msg->twist);
+	twist = odom_msg->twist;
+	geometry_msgs::Vector3 twist_linear = twist.twist.linear;
+	geometry_msgs::Vector3 twist_orient = twist.twist.angular;
+	// twistCov = twist.covariance;
+	// ROS_INFO("twist linear: (%f, %f, %f)", twist_linear.x, twist_linear.y, twist_linear.z);
+	// ROS_INFO("twist orient: (%f, %f, %f)", twist_orient.x, twist_orient.y, twist_orient.z);
 
 }
 
